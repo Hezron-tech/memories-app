5e7016702159bc99a8d6c54989e9d467eed365d7
Hezron-tech <kiprophez@gmail.com>
2023-02-16 11:42:10 +0300

    working on client side

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,42 @@
-import React from 'react'
+import { Container, AppBar, Typography, Grow, Grid } from "@material-ui/core";
+// import memories from "./images/memories.jpeg";
+import Form from "./components/Form/Form";
+import Posts from "./components/Posts/posts";
 
-const App=()=>{
+import useStyles from './styles'
 
-    return(
-        <div>
-            <h1>App</h1>
-        </div>
-    )
+function App() {
 
+  const classes = useStyles()
+  return (
+    <Container maxWidth="lg">
+      <AppBar className={classes.appBar} position="static" color="inherit">
+        <Typography  className={classes.heading} variant="h2" align="center">
+          Memories
+        </Typography>
+
+        <img className={classes.image} src={memories} alt="memories" height="60" />
+      </AppBar>
+
+      <Grow in>
+        <Container>
+          <Grid
+            container
+            justifyContent="space-between"
+            alignItems="stretch"
+            spacing={3}
+          >
+            <Grid item xs={12} sm={7}>
+              <Posts />
+            </Grid>
+            <Grid item xs={12} sm={4}>
+              <Form />
+            </Grid>
+          </Grid>
+        </Container>
+      </Grow>
+    </Container>
+  );
 }
 
-export default App
+export default App;
---
+++ client/src/components/Posts/posts.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import Post from './post/Post'
+
+const Posts =()=>{
+    return(
+        <div>
+            <h1>Posts</h1>
+
+            <Post/>
+        </div>
+    )
+}
+
+export default Posts
