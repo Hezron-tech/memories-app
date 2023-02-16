08eb27c192265401433a1d71c8221eb6b887adc7
Hezron-tech <kiprophez@gmail.com>
2023-02-16 12:34:20 +0300

    fetching data using state

--- client/src/actions/posts.js
+++ client/src/actions/posts.js
@@ -2,3 +2,20 @@ import * as api from '../api'
 
 //action creators
 
+
+
+const getPosts = ()=> async (dispatch)=>{
+
+    try {
+        
+
+        const {data}= await api.fetchPosts()
+        dispatch({ type: 'FETCH_ALL',payload : data})
+    } catch (error) {
+
+        console.log(error.message);
+    }
+    
+
+    dispatch(action)
+}
