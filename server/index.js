642be95d58f194c2f46ccaae76962341a5887a28
Hezron-tech <kiprophez@gmail.com>
2023-02-16 10:32:21 +0300

    work on creating server

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ import express from 'express'
 import mongoose from 'mongoose'
 import cors from 'cors'
 import dotenv from 'dotenv'
-
+import bodyParser from 'body-parser'
 dotenv.config()
 
 
@@ -11,14 +11,23 @@ dotenv.config()
 
 const app = express()
 app.use(cors())
-app.use(express.json())
+app.use(bodyParser.json({limit:"30mb",extended:true}))
+app.use(bodyParser.urlencoded({limit:"30mb",extended:true}))
 
 const port =process.env.PORT || 30000
 
-app.use('/',(req,res)=>{
-    res.send('Memories app')
-})
+mongoose.connect(process.env.MONGO_URL ,{useNewUrlParser:true,useUnifiedTopology:true})
+           .then(()=>{
+            app.listen(port, ()=>{
+                console.log(`server is running on port ${port}`);
+            })
+           })
+           .catch((error)=>{
+            console.log(error.message);
 
-app.listen(port, ()=>{
-    console.log(`server is running on port ${port}`);
-})
+           })
+
+           
+
+
+
